079d23e7ca07c1b772074a77a799b716cc5fb299
Cristiano Gotardi <crgotardi@gmail.com>
2021-07-08 14:04:59 -0300

    App 13 About me

---
+++ App13_AboutMe/app/src/main/java/com/cristiano/app13_aboutme/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.cristiano.app13_aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var menu: TabLayout
+    lateinit var visualizador: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        menu = findViewById(R.id.tblMenu)
+        visualizador = findViewById(R.id.vpgVisualizador)
+
+        visualizador.adapter = PagerAdapter(supportFragmentManager)
+        menu.setupWithViewPager(visualizador)
+    }
+}
